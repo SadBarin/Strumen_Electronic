b0f2f9bcd6cedfc51850b361558b51baf22510bc
SadBarin <SadBarin@yandex.com>
2021-04-19 20:28:54 +0300

    add logic gates info in popup info

--- src/components/layout/popups/popup-info/popup-info.jsx
+++ src/components/layout/popups/popup-info/popup-info.jsx
@@ -9,7 +9,76 @@ function PopupInfo({ hidden, closePopup }) {
     <PopupWrapper closePopup={closePopup} hidden={hidden} title="Информация о логике">
       <div>
         <p><b>OR</b> - логическое ИЛИ</p>
+        <table>
+          <tbody>
+            <tr>
+              <td colSpan="2"><b>Input</b></td>
+              <td><b>Output</b>
+              </td>
+            </tr>
+            <tr>
+              <td>A</td>
+              <td>B</td>
+              <td align="center">A OR B</td>
+            </tr>
+            <tr>
+              <td>0</td>
+              <td>0</td>
+              <td align="center">0</td>
+            </tr>
+            <tr>
+              <td>0</td>
+              <td>1</td>
+              <td align="center">1</td>
+            </tr>
+            <tr>
+              <td>1</td>
+              <td>0</td>
+              <td align="center">1</td>
+            </tr>
+            <tr>
+              <td>1</td>
+              <td>1</td>
+              <td align="center">1</td>
+            </tr>
+          </tbody>
+        </table>
+
         <p><b>AND</b> - логическое И</p>
+        <table>
+          <tbody>
+            <tr>
+              <td colSpan="2"><b>Input</b></td>
+              <td><b>Output</b>
+              </td>
+            </tr>
+            <tr>
+              <td>A</td>
+              <td>B</td>
+              <td align="center">A AND B</td>
+            </tr>
+            <tr>
+              <td>0</td>
+              <td>0</td>
+              <td align="center">0</td>
+            </tr>
+            <tr>
+              <td>0</td>
+              <td>1</td>
+              <td align="center">0</td>
+            </tr>
+            <tr>
+              <td>1</td>
+              <td>0</td>
+              <td align="center">0</td>
+            </tr>
+            <tr>
+              <td>1</td>
+              <td>1</td>
+              <td align="center">1</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     </PopupWrapper>
   );
