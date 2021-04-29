fec75d8acb26d1d0939f58352f8ab1138f8f08cd
SadBarin <SadBarin@yandex.com>
2021-04-30 00:13:54 +0300

    add service worker

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
 ReactDOM.render(
   <App />, document.getElementById('root'),
 );
+
+serviceWorkerRegistration.unregister();
