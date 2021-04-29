c74fb271050b2d4da5d08057a0fb0c95c9681d2a
SadBarin <SadBarin@yandex.com>
2021-04-29 23:13:07 +0300

    add adaptive for popup

--- src/components/layout/popups/popup-info/popup-info.jsx
+++ src/components/layout/popups/popup-info/popup-info.jsx
@@ -6,56 +6,103 @@ import PopupWrapper from '../popup-wrapper';
 
 function PopupInfo({ hidden, closePopup }) {
   return (
-    <PopupWrapper closePopup={closePopup} hidden={hidden} title="Информация о логике">
-      <div>
-        <p><b>OR</b> - логическое ИЛИ</p>
-        <table>
-          <tbody>
+    <PopupWrapper closePopup={closePopup} hidden={hidden} title="Таблицы истинности">
+      <div className="popup-info-content">
+        <div>
+          <h3>Логическое ИЛИ</h3>
+          <table>
+            <thead>
             <tr>
-              <td colSpan="2"><b>Input</b></td>
-              <td><b>Output</b>
-              </td>
+              <th colSpan="2"><b>Input</b></th>
+              <th><b>Output</b></th>
             </tr>
-            <tr>
-              <td>A</td>
-              <td>B</td>
-              <td align="center">A OR B</td>
-            </tr>
-            <tr>
-              <td>0</td>
-              <td>0</td>
-              <td align="center">0</td>
-            </tr>
-            <tr>
-              <td>0</td>
-              <td>1</td>
-              <td align="center">1</td>
-            </tr>
-            <tr>
-              <td>1</td>
-              <td>0</td>
-              <td align="center">1</td>
-            </tr>
-            <tr>
-              <td>1</td>
-              <td>1</td>
-              <td align="center">1</td>
-            </tr>
-          </tbody>
-        </table>
+            </thead>
 
-        <p><b>AND</b> - логическое И</p>
-        <table>
-          <tbody>
+            <tbody>
+              <tr>
+                <td>A</td>
+                <td>B</td>
+                <td align="center">A OR B</td>
+              </tr>
+              <tr>
+                <td>0</td>
+                <td>0</td>
+                <td align="center">0</td>
+              </tr>
+              <tr>
+                <td>0</td>
+                <td>1</td>
+                <td align="center">1</td>
+              </tr>
+              <tr>
+                <td>1</td>
+                <td>0</td>
+                <td align="center">1</td>
+              </tr>
+              <tr>
+                <td>1</td>
+                <td>1</td>
+                <td align="center">1</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+
+        <div>
+          <h3>Логическое И</h3>
+          <table>
+            <thead>
             <tr>
-              <td colSpan="2"><b>Input</b></td>
-              <td><b>Output</b>
-              </td>
+              <th colSpan="2"><b>Input</b></th>
+              <th><b>Output</b>
+              </th>
             </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>A</td>
+                <td>B</td>
+                <td align="center">A AND B</td>
+              </tr>
+              <tr>
+                <td>0</td>
+                <td>0</td>
+                <td align="center">0</td>
+              </tr>
+              <tr>
+                <td>0</td>
+                <td>1</td>
+                <td align="center">0</td>
+              </tr>
+              <tr>
+                <td>1</td>
+                <td>0</td>
+                <td align="center">0</td>
+              </tr>
+              <tr>
+                <td>1</td>
+                <td>1</td>
+                <td align="center">1</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+
+        <div>
+          <h3>Исключающее ИЛИ</h3>
+          <table>
+            <thead>
+            <tr>
+              <th colSpan="2"><b>Input</b></th>
+              <th><b>Output</b>
+              </th>
+            </tr>
+            </thead>
+            <tbody>
             <tr>
               <td>A</td>
               <td>B</td>
-              <td align="center">A AND B</td>
+              <td align="center">A XOR B</td>
             </tr>
             <tr>
               <td>0</td>
@@ -65,11 +112,51 @@ function PopupInfo({ hidden, closePopup }) {
             <tr>
               <td>0</td>
               <td>1</td>
-              <td align="center">0</td>
+              <td align="center">1</td>
             </tr>
             <tr>
               <td>1</td>
               <td>0</td>
+              <td align="center">1</td>
+            </tr>
+            <tr>
+              <td>1</td>
+              <td>1</td>
+              <td align="center">0</td>
+            </tr>
+            </tbody>
+          </table>
+        </div>
+
+        <div>
+          <h3>Исключающее ИЛИ-НЕ</h3>
+          <table>
+            <thead>
+            <tr>
+              <th colSpan="2"><b>Input</b></th>
+              <th><b>Output</b>
+              </th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+              <td>A</td>
+              <td>B</td>
+              <td align="center">A XNOR B</td>
+            </tr>
+            <tr>
+              <td>0</td>
+              <td>0</td>
+              <td align="center">1</td>
+            </tr>
+            <tr>
+              <td>1</td>
+              <td>0</td>
+              <td align="center">0</td>
+            </tr>
+            <tr>
+              <td>0</td>
+              <td>1</td>
               <td align="center">0</td>
             </tr>
             <tr>
@@ -77,8 +164,9 @@ function PopupInfo({ hidden, closePopup }) {
               <td>1</td>
               <td align="center">1</td>
             </tr>
-          </tbody>
-        </table>
+            </tbody>
+          </table>
+        </div>
       </div>
     </PopupWrapper>
   );
